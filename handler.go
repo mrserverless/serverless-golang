8034df2f40e2c4320f7e423b25a9a6beb7379175
Cristian Măgherușan-Stanciu <cristian.magherusan-stanciu@here.com>
2017-01-16 15:04:19 +0100

    fix port to aws-lambda-go-core

--- handler.go
+++ handler.go
@@ -3,10 +3,10 @@ package main
 import "C"
 
 import (
-  "encoding/json"
-  "github.com/eawsy/aws-lambda-go/service/lambda/runtime"
+	"encoding/json"
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
 )
 
 func Handle(evt json.RawMessage, ctx *runtime.Context) (interface{}, error) {
-  return "Hello, World!", nil
+	return "Hello, World!", nil
 }
